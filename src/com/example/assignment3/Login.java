71b6e63568cbe863448b6975013c46e16ae2d49d
Craig LeClair <craigleclair4@gmail.com>
2013-12-01 00:59:45 -0800

    Some changes to Login.

--- src/com/example/assignment3/Login.java
+++ src/com/example/assignment3/Login.java
@@ -1,8 +1,11 @@
 package com.example.assignment3;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends Activity {
 
@@ -10,6 +13,26 @@ public class Login extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        navigateToRegister();
+
+        Intent loginIntent = new Intent(this, ListQuestions.class);
+        EditText usernameEdit = (EditText) findViewById(R.id.username_entry);
+        EditText passwordEdit = (EditText) findViewById(R.id.password_entry);
+
+        String username = usernameEdit.getText().toString();
+        String password = passwordEdit.getText().toString();
+
+    }
+
+    public void navigateToRegister() {
+        Intent registerIntent = new Intent(this, Register.class);
+
+        Button registerButton = (Button) findViewById(R.id.register_button);
+
+        // registerButton.setOnClickListener(new OnClickListener() {
+        //
+        // })
     }
 
     @Override
