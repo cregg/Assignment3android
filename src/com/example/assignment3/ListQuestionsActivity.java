a04ea139c273cbb78dadd2146b08778e80a9e15e
Craig LeClair <craigleclair4@gmail.com>
2013-12-02 01:59:58 -0800

    Code to get Selected RadioButtons

--- src/com/example/assignment3/ListQuestionsActivity.java
+++ src/com/example/assignment3/ListQuestionsActivity.java
@@ -18,46 +18,61 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SimpleAdapter;
 
 public class ListQuestionsActivity extends Activity {
-	private ProgressDialog pDialog;
-	ListView list;
-	private String week;
-	private String weekNo;
-	private String quizID;
-	private ViewGroup buttons;
-	ArrayList<HashMap<String, String>> oslist = new ArrayList<HashMap<String, String>>();
-	
-	private static final String TAG_QUIZ = "quiz";
-	private static final String TAG_QUIZID = "quizID";
-	private static final String TAG_WEEKNO = "weekNo";
-	private static final String TAG_QUESTIONS = "questions";
-	private static final String TAG_ANSWERS = "answers";
-	private static final String TAG_ANSWERID = "answerID";
-	private static final String TAG_ANSWER = "answer";
-	private static final String TAG_ANSWER1 = "answer1";
-	private static final String TAG_ANSWER2 = "answer2";
-	private static final String TAG_ANSWER3 = "answer3";
-	private static final String TAG_ANSWER4 = "answer4";
-	private static final String TAG_QUESTIONID = "questionID";
-	private static final String TAG_QUESTION = "question";
-	
-	JSONArray android = null;
-	JSONObject json;
-	
+    private ProgressDialog pDialog;
+    ListView list;
+    private String week;
+    private String weekNo;
+    private String quizID;
+    private ViewGroup buttons;
+    private RadioGroup tyorke;
+    ArrayList<HashMap<String, String>> oslist = new ArrayList<HashMap<String, String>>();
+
+    private static final String TAG_QUIZ = "quiz";
+    private static final String TAG_QUIZID = "quizID";
+    private static final String TAG_WEEKNO = "weekNo";
+    private static final String TAG_QUESTIONS = "questions";
+    private static final String TAG_ANSWERS = "answers";
+    private static final String TAG_ANSWERID = "answerID";
+    private static final String TAG_ANSWER = "answer";
+    private static final String TAG_ANSWER1 = "answer1";
+    private static final String TAG_ANSWER2 = "answer2";
+    private static final String TAG_ANSWER3 = "answer3";
+    private static final String TAG_ANSWER4 = "answer4";
+    private static final String TAG_QUESTIONID = "questionID";
+    private static final String TAG_QUESTION = "question";
+
+    JSONArray android = null;
+    JSONObject json;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_questions);
         try {
-        json = new JSONObject(getIntent().getStringExtra("jsonObject"));
-        populateView(json);
-        } catch(Exception e) {
-        	e.printStackTrace();
+            json = new JSONObject(getIntent().getStringExtra("jsonObject"));
+            populateView(json);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-     
+
         Button submitButton = (Button) findViewById(R.id.submit_answers);
+
+        /**
+         * This gets the id of the checked radio button and then grabs the radio
+         * button. I don't know what you plan to do with it after.
+         */
+        for (int i = 0; i < list.getChildCount(); i++) {
+            if (list.getChildAt(i) instanceof RadioGroup) {
+                tyorke = (RadioGroup) list.getChildAt(i);
+                int id = tyorke.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) findViewById(id);
+            }
+        }
 
         submitButton.setOnClickListener(new View.OnClickListener() {
 
@@ -67,61 +82,60 @@ public class ListQuestionsActivity extends Activity {
             }
         });
     }
-    
+
     protected void populateView(JSONObject json) {
-		try {
-			JSONObject o = json.getJSONObject(TAG_QUIZ);
-			quizID = o.getString(TAG_QUIZID);
-			weekNo = o.getString(TAG_WEEKNO);
+        try {
+            JSONObject o = json.getJSONObject(TAG_QUIZ);
+            quizID = o.getString(TAG_QUIZID);
+            weekNo = o.getString(TAG_WEEKNO);
 
-			// Getting JSON Array from URL
-			android = json.getJSONArray(TAG_QUESTIONS);
+            // Getting JSON Array from URL
+            android = json.getJSONArray(TAG_QUESTIONS);
 
-			for (int i = 0; i < android.length(); i++) {
-				JSONObject c = android.getJSONObject(i);
-				JSONObject q = c.getJSONObject(TAG_QUESTION);
-				String question = q.getString(TAG_QUESTION);
+            for (int i = 0; i < android.length(); i++) {
+                JSONObject c = android.getJSONObject(i);
+                JSONObject q = c.getJSONObject(TAG_QUESTION);
+                String question = q.getString(TAG_QUESTION);
 
-				JSONArray qObject = c.getJSONArray(TAG_ANSWERS);
+                JSONArray qObject = c.getJSONArray(TAG_ANSWERS);
 
-				JSONObject a = qObject.getJSONObject(0);
-				String answer1 = a.getString(TAG_ANSWER);
-				a = qObject.getJSONObject(1);
-				String answer2 = a.getString(TAG_ANSWER);
-				a = qObject.getJSONObject(2);
-				String answer3 = a.getString(TAG_ANSWER);
-				a = qObject.getJSONObject(3);
-				String answer4 = a.getString(TAG_ANSWER);
+                JSONObject a = qObject.getJSONObject(0);
+                String answer1 = a.getString(TAG_ANSWER);
+                a = qObject.getJSONObject(1);
+                String answer2 = a.getString(TAG_ANSWER);
+                a = qObject.getJSONObject(2);
+                String answer3 = a.getString(TAG_ANSWER);
+                a = qObject.getJSONObject(3);
+                String answer4 = a.getString(TAG_ANSWER);
 
-				HashMap<String, String> map = new HashMap<String, String>();
+                HashMap<String, String> map = new HashMap<String, String>();
 
-				map.put(TAG_QUESTION, question);
-				map.put(TAG_ANSWER1, answer1);
-				map.put(TAG_ANSWER2, answer2);
-				map.put(TAG_ANSWER3, answer3);
-				map.put(TAG_ANSWER4, answer4);
+                map.put(TAG_QUESTION, question);
+                map.put(TAG_ANSWER1, answer1);
+                map.put(TAG_ANSWER2, answer2);
+                map.put(TAG_ANSWER3, answer3);
+                map.put(TAG_ANSWER4, answer4);
 
-				oslist.add(map);
-				list = (ListView) findViewById(R.id.list);
+                oslist.add(map);
+                list = (ListView) findViewById(R.id.list);
 
-				ListAdapter adapter = new SimpleAdapter(
-						ListQuestionsActivity.this, oslist,
-						R.layout.list_member, new String[] { TAG_QUESTION,
-								TAG_ANSWER1, TAG_ANSWER2, TAG_ANSWER3,
-								TAG_ANSWER4 }, new int[] { R.id.question,
-								R.id.answer1, R.id.answer2, R.id.answer3,
-								R.id.answer4 });
+                ListAdapter adapter = new SimpleAdapter(
+                        ListQuestionsActivity.this, oslist,
+                        R.layout.list_member, new String[] { TAG_QUESTION,
+                                TAG_ANSWER1, TAG_ANSWER2, TAG_ANSWER3,
+                                TAG_ANSWER4 }, new int[] { R.id.question,
+                                R.id.answer1, R.id.answer2, R.id.answer3,
+                                R.id.answer4 });
 
-				if(list == null)
-					System.out.println("null");
-				 
-				 
-				list.setAdapter(adapter);
+                if (list == null)
+                    System.out.println("null");
 
-			}
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
+                list.setAdapter(adapter);
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -142,24 +156,23 @@ public class ListQuestionsActivity extends Activity {
         protected void onPreExecute() {
             super.onPreExecute();
             try {
-            	v = (ListView) findViewById(R.id.list);
-            	j = new JSONObject();
-            	temp = new JSONObject();
-	            temp.put(TAG_QUIZID, quizID);
-	            temp.put(TAG_WEEKNO, weekNo);
-	            j.put(TAG_QUIZ, temp);
-	            
-	            n = new JSONArray();
-	            temp = new JSONObject();
-	            
-	            
-	            pDialog = new ProgressDialog(ListQuestionsActivity.this);
-	            pDialog.setMessage("Computing Scores ...");
-	            pDialog.setIndeterminate(false);
-	            pDialog.setCancelable(true);
-	            pDialog.show();
-            } catch(Exception e) {
-            	e.printStackTrace();
+                v = (ListView) findViewById(R.id.list);
+                j = new JSONObject();
+                temp = new JSONObject();
+                temp.put(TAG_QUIZID, quizID);
+                temp.put(TAG_WEEKNO, weekNo);
+                j.put(TAG_QUIZ, temp);
+
+                n = new JSONArray();
+                temp = new JSONObject();
+
+                pDialog = new ProgressDialog(ListQuestionsActivity.this);
+                pDialog.setMessage("Computing Scores ...");
+                pDialog.setIndeterminate(false);
+                pDialog.setCancelable(true);
+                pDialog.show();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
         }
