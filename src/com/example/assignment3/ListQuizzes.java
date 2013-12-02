959f18466a7542790216a3815f84dc67e390e40f
Craig LeClair <craigleclair4@gmail.com>
2013-12-01 16:34:22 -0800

    Navigates from quiz choice to questions.

--- src/com/example/assignment3/ListQuizzes.java
+++ src/com/example/assignment3/ListQuizzes.java
@@ -1,8 +1,11 @@
 package com.example.assignment3;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class ListQuizzes extends Activity {
 
@@ -10,6 +13,33 @@ public class ListQuizzes extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_quizes);
+
+        Button[] btns = new Button[14];
+
+        btns[0] = (Button) findViewById(R.id.quiz1);
+        btns[1] = (Button) findViewById(R.id.quiz2);
+        btns[2] = (Button) findViewById(R.id.quiz3);
+        btns[3] = (Button) findViewById(R.id.quiz4);
+        btns[4] = (Button) findViewById(R.id.quiz5);
+        btns[5] = (Button) findViewById(R.id.quiz6);
+        btns[6] = (Button) findViewById(R.id.quiz7);
+        btns[7] = (Button) findViewById(R.id.quiz8);
+        btns[8] = (Button) findViewById(R.id.quiz9);
+        btns[9] = (Button) findViewById(R.id.quiz10);
+        btns[10] = (Button) findViewById(R.id.quiz11);
+        btns[11] = (Button) findViewById(R.id.quiz12);
+        btns[12] = (Button) findViewById(R.id.quiz13);
+
+        for (int i = 0; i < btns.length - 1; i++) {
+            btns[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent in = new Intent(getApplicationContext(),
+                            ListQuestionsActivity.class);
+                    startActivity(in);
+                }
+            });
+        }
     }
 
     @Override
@@ -17,6 +47,7 @@ public class ListQuizzes extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.list_quizes, menu);
         return true;
+
     }
 
 }
